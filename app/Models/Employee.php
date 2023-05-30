b616230cf4502fabf341dde8fe67c75a5672f924
TerminalDZ <boukemoucheidriss@gmail.com>
2023-05-30 07:57:37 +0100

    Up  resource

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = 
+    [
+    'first_name',
+    'last_name',
+    'address',
+    'city_id',
+    'state_id',
+    'country_id',
+    'department_id',
+    'zip_code',
+    'birth_date',
+    'date_hires'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
